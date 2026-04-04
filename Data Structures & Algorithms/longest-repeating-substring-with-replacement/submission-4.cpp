fbffe67199a0062e8610f84b04b7c003b85bb642
Simon Cun <simoncun2586@gmail.com>
2026-04-04 16:22:26 -0700

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.cpp
@@ -0,0 +1,21 @@
+#include <unordered_map>
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char, int> hashMap;
+        int l = 0;
+        int res = 0;
+        int maxFreq = 0;
+        for (int r = 0; r < s.size(); ++r) {
+            hashMap[s[r]] += 1;
+            maxFreq = max(maxFreq, hashMap[s[r]]);
+            
+            while ((r - l) - maxFreq >= k) {
+                --hashMap[s[l]];
+                ++l;
+            }
+            res = max(res, r - l + 1);
+        }
+        return res;
+    }
+};
