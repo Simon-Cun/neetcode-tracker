2fd93503398bec5a368876021e3cc61be36e569c
Simon Cun <simoncun2586@gmail.com>
2026-04-04 18:22:52 -0700

    Add: minimum-window-with-characters - submission-29

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-29.cpp
@@ -0,0 +1,38 @@
+#include <unordered_map>
+#include <string>
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> tFreq;
+        for (auto& i : t) {
+            ++tFreq[i];
+        }
+        int have = 0;
+        int l = 0;
+        string res = "";
+        unordered_map<char, int> sFreq;
+        for (int r = 0; r < s.size(); ++r) {
+            if (tFreq.contains(s[r])) {
+                if (sFreq[s[r]] < tFreq[s[r]]) {
+                    ++have;
+                }
+                ++sFreq[s[r]];
+            }
+            if (have == t.size()) {
+                while (have == t.size()) {
+                    if (res.empty() || res.size() > (r - l)) {
+                        res = s.substr(l, r - l + 1);
+                    }
+                    if (tFreq.contains(s[l])) {
+                        if (sFreq[s[l]] <= tFreq[s[l]]) {
+                            --have;
+                        }
+                        --sFreq[s[l]];
+                    }
+                    ++l;
+                }
+            }
+        }
+        return res;
+    }
+};
