29532c17d23b021808bff80629ac835c2497a90e
Simon Cun <simoncun2586@gmail.com>
2026-04-04 21:02:27 -0700

    Add: minimum-stack - submission-14

---
+++ Data Structures & Algorithms/minimum-stack/submission-14.cpp
@@ -0,0 +1,30 @@
+#include <stack>
+class MinStack {
+private:
+    stack<int> st;
+    stack<int> minSt;
+public:
+    MinStack() {}
+    
+    void push(int val) {
+        st.push(val);
+        if (minSt.empty() || minSt.top() >= val) {
+            minSt.push(val);
+        }
+    }
+    
+    void pop() {
+        if (st.top() == minSt.top()) {
+            minSt.pop();
+        }
+        st.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minSt.top();
+    }
+};
