8d648ee1467c2cfa0a1cde4d9435fc4e544f656f
Simon Cun <simoncun2586@gmail.com>
2026-04-09 17:02:44 -0700

    Add: duplicate-integer - submission-12

---
+++ Data Structures & Algorithms/duplicate-integer/submission-12.cpp
@@ -0,0 +1,11 @@
+#include <unordered_set>
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for (auto& n : nums) {
+            s.insert(n);
+        }
+        return s.size() != nums.size();
+    }
+};
