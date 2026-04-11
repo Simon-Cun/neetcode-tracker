2e04d56fe5810330688145f3b85925a8e2072b37
Simon Cun <simoncun2586@gmail.com>
2026-04-11 12:28:32 -0700

    Add: permutation-string - submission-16

---
+++ Data Structures & Algorithms/permutation-string/submission-16.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        vector<int> s1Chars(26, 0);
+        for (const auto& i : s1) {
+            ++s1Chars.at(i - 'a');
+        }
+        int l = 0;
+        vector<int> s2Chars(26, 0);
+        for (int r = 0; r < s2.size(); ++r) {
+            ++s2Chars.at(s2.at(r) - 'a');
+            if (r - l == s1.size()) {
+                --s2Chars.at(s2.at(l) - 'a');
+                ++l;
+            }
+            if (s2Chars == s1Chars) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
