d1ddd6bdc4281914b1c01be1240e6b61c01d297e
Simon Cun <simoncun2586@gmail.com>
2026-04-10 18:40:21 -0700

    Add: longest-consecutive-sequence - submission-51

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-51.cpp
@@ -0,0 +1,22 @@
+#include <unordered_set>
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> values;
+        for (const auto& i : nums) {
+            values.insert(i);
+        }
+        int ret = 0;
+        for (const auto& i : values) {
+            if (!values.contains(i - 1)) {
+                int count = 0;
+                int curr = i;
+                while (values.contains(curr++)) {
+                    ++count;
+                }
+                ret = max(ret, count);
+            }
+        }
+        return ret;
+    }
+};
