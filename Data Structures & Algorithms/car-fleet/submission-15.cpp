41c83e262d54f9b18bde44a421398b7fdec19cc5
Simon Cun <simoncun2586@gmail.com>
2026-04-15 17:38:55 -0700

    Add: car-fleet - submission-15

---
+++ Data Structures & Algorithms/car-fleet/submission-15.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int, int>> posSpe;
+        for (int i = 0; i < position.size(); ++i) {
+            posSpe.push_back({position.at(i), speed.at(i)});
+        }
+        sort(posSpe.begin(), posSpe.end());
+        stack<double> st;
+        for (int i = 0; i < posSpe.size(); ++i) {
+            double curr = (target - posSpe.at(i).first) / (double)posSpe.at(i).second;
+            cout << curr << endl;
+            while (!st.empty() && st.top() <= curr) {
+                st.pop();
+            }
+            st.push(curr);
+        }
+        return st.size();
+    }
+};
