9dfd8cad0a76fc3b5c8b5c63dfb2a7c31037797a
Simon Cun <simoncun2586@gmail.com>
2026-04-20 22:18:52 -0700

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int bitmask = 0;
+        for (auto& i : nums) {
+            cout << bitset<16>(bitmask) << endl;
+            if (((1 << i) & bitmask) != 0) {
+                return i;
+            } else {
+                bitmask |= (1 << i);
+            }
+        }
+        return -1;
+    }
+};
