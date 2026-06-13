4a1ecaeb1ed2770f475ab401e90d299890cd5020
Simon Cun <simoncun2586@gmail.com>
2026-06-12 18:58:30 -0700

    Add: top-k-elements-in-list - submission-20

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-20.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> hashMap;
+        for (auto& i : nums) ++hashMap[i];
+
+        vector<vector<int>> buckets(nums.size() + 1);
+        for (auto& i : hashMap) buckets.at(i.second).push_back(i.first);
+        reverse(buckets.begin(), buckets.end());
+        vector<int> ret;
+        for (auto& i : buckets) {
+            for (auto& j : i) {
+                if (ret.size() == k) break;
+                else ret.push_back(j);
+            }
+        }
+        return ret;
+    }
+};
