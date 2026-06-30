6655cfc4c92495cb47b77636204d38c2a98a8c89
Simon Cun <simoncun2586@gmail.com>
2026-06-29 17:17:49 -0700

    Add: string-encode-and-decode - submission-33

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-33.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string ret = "";
+        for (auto& i : strs) {
+            ret +=  to_string(i.size()) + '|' + i;
+        }
+        return ret;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> ret;
+        string num = "";
+        for (int i = 0; i < s.size(); ++i) {
+            if (s.at(i) != '|') num += s.at(i);
+            else {
+                ret.push_back(s.substr(i + 1, stoi(num)));
+                i += stoi(num);
+                num = "";
+            }
+        }
+        return ret;
+    }
+};
