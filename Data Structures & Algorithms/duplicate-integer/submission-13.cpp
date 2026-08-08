e6b948f9af62d3920587c178fecf02aff7313ece
Simon Cun <simoncun2586@gmail.com>
2026-08-08 10:48:51 -0700

    Update: duplicate-integer - submission-13

--- Data Structures & Algorithms/duplicate-integer/submission-13.cpp
+++ Data Structures & Algorithms/duplicate-integer/submission-13.cpp
@@ -1,14 +1,9 @@
-#include <unordered_map>
 class Solution {
 public:
     bool hasDuplicate(vector<int>& nums) {
-        std::unordered_map<int, int> hashMap;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (hashMap.contains(nums.at(i))) {
-                return true;
-            }
-            hashMap[nums[i]] = 0;
-        }
+        unordered_map<int, int> hash;
+        for (auto& i : nums) hash[i]++;
+        for (auto& i : hash) if (i.second != 1) return true;
         return false;
     }
 };
